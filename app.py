e1114e1a34a45fb2767d2a07779204652fbeb504
kberezynets <kberezynets@gmail.com>
2024-10-19 14:16:36 +0300

    Task 5 commit: division added

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-# Task 2
-# Create a file app.py and write a CLI app - a simple calculator: 
-# it should be able to add and subtract two numbers.
+# Task 5
+# Return to the main branch and create a new branch feat-div. 
+# Checkout this branch and develop division support for your app. Don't forget to commit your changes.
 
 def CLI(x: int, z: int):
-    return x + y
+    match operation:
+        case '+':
+            return x + y
+        case '-':
+            return x - y
+        case '*':
+            return x * y
+        case '/':
+            return x / y
+        case _:
+            return 'Unrecognised command'
